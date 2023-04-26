c34923d110c43ed5a9ab4c31153cbb1fb3c5ad56
Esteban <estebanyalg@gmail.com>
2023-04-26 11:52:14 -0500

    Adding Simple Gretting

--- main.go
+++ main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
-func webPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hi team!")
+type Greeting struct {
+	Name string
 }
 
 func main() {
-	http.HandleFunc("/", webPage)
-	err := http.ListenAndServe(":3333", nil)
-	if err != nil {
-		log.Fatal(err)
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/greet", handleGreet)
+	log.Fatal(http.ListenAndServe(":3333", nil))
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func handleGreet(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("name")
+	greeting := Greeting{Name: name}
+	tmpl := template.Must(template.ParseFiles("greet.html"))
+	if err := tmpl.Execute(w, greeting); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
