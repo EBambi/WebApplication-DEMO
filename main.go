2bb444fe2bbc0b276428ddb15b61a23257310b83
Esteban <estebanyalg@gmail.com>
2023-05-30 18:31:11 -0500

    Change application and add unit testing

--- main.go
+++ main.go
@@ -1,33 +1,78 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
-	"log"
 	"net/http"
+	"sort"
+	"strconv"
+	"strings"
 )
 
-type Greeting struct {
-	Name string
+type TemplateData struct {
+	InputNumbers  string
+	SortedNumbers []int
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.Execute(w, nil)
+	} else if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		input := strings.TrimSpace(r.Form.Get("numbers"))
+		numbers := parseNumbers(input)
+		sortedNumbers := orderNumbers(numbers)
+
+		data := TemplateData{
+			InputNumbers:  input,
+			SortedNumbers: sortedNumbers,
+		}
+
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.Execute(w, data)
+	}
+}
+
+func parseNumbers(input string) []int {
+	numStrs := strings.Split(input, ",")
+	numbers := make([]int, 0, len(numStrs))
+	for _, numStr := range numStrs {
+		num, err := strconv.Atoi(strings.TrimSpace(numStr))
+		if err != nil {
+			continue
+		}
+		numbers = append(numbers, num)
+	}
+	return numbers
+}
+
+func orderNumbers(numbers []int) []int {
+	sortedNumbers := make([]int, len(numbers))
+	copy(sortedNumbers, numbers)
+	sort.Ints(sortedNumbers)
+	return sortedNumbers
+}
+
+func testEq(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
-	http.HandleFunc("/", handleIndex)
-	http.HandleFunc("/greet", handleGreet)
-	log.Fatal(http.ListenAndServe(":3333", nil))
-}
-
-func handleIndex(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("index.html"))
-	if err := tmpl.Execute(w, nil); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func handleGreet(w http.ResponseWriter, r *http.Request) {
-	name := r.FormValue("name")
-	greeting := Greeting{Name: name}
-	tmpl := template.Must(template.ParseFiles("greet.html"))
-	if err := tmpl.Execute(w, greeting); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+	http.HandleFunc("/", homeHandler)
+	fmt.Println("Server started on port 8080")
+	http.ListenAndServe(":8080", nil)
 }
