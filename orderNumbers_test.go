467898ff12a747389bb1d920f7f5a4746810f6ed
Esteban <estebanyalg@gmail.com>
2023-06-07 13:28:47 -0500

    Unit testing pass

--- orderNumbers_test.go
+++ orderNumbers_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestOrderNumbers(t *testing.T) {
 	toOrder := []int{8, 20, 2, 53, 9, 101, 3, 234}
 	got := orderNumbers(toOrder)
-	want := []int{3, 2, 8, 9, 20, 53, 101, 234}
+	want := []int{2, 3, 8, 9, 20, 53, 101, 234}
 
 	if testEq(got, want) == false {
 		t.Errorf("Got %q, wanted %q", got, want)
